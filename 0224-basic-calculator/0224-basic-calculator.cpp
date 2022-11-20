05d2520c11af0d8781f4ebd171a2c4eca6874ac7
aamirafzal0007 <103625204+aamirafzal0007@users.noreply.github.com>
2022-11-21 00:00:27 +0530

    Time: 25 ms (47.20%), Space: 8.2 MB (43.51%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.cpp
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -1,38 +1,52 @@
 class Solution {
 public:
     int calculate(string s) {
-       stack<int> st;
-        int number = 0;
-        int result = 0;
-        int sign   = 1;
-        
-        for(int i = 0; i<s.length(); i++) {
-            if(isdigit(s[i])) {
-                number  = 10*number + (s[i] - '0');
-            } else if(s[i] == '+') {
-                result += sign*number;
-                number  = 0;
-                sign = 1; //For further
-            } else if(s[i] == '-') {
-                result += sign*number;
-                number  = 0;
-                sign = -1; //For further
-            } else if(s[i] == '(') {
-                st.push(result);
-                st.push(sign);
-                result = 0;
-                number = 0;
-                sign = 1;
-            } else if(s[i] == ')') {
-                result += sign*number;
-                number = 0;
-                int top = st.top(); st.pop();
-                result *= top;
-                top = st.top(); st.pop();
-                result += top;
+        int num=0;
+        int res=0;
+        int sign=1;
+        int n=s.length();
+        stack<int>st;
+        for(int i=0;i<n;i++)
+        {
+            if(isdigit(s[i]))
+            {
+                num=num*10+(s[i]-'0');
             }
+            else if(s[i]=='+')
+            {
+                res+=(num*sign);
+                num=0;
+                sign=1;
+            }
+            else if(s[i]=='-')
+            {
+                res+=(num*sign);
+                num=0;
+                sign=-1;
+            }
+            else if(s[i]=='(')
+            {
+                st.push(res);
+                st.push(sign);
+                sign=1;
+                res=0;
+                num=0;
+            }
+            else if(s[i]==')')
+            {
+                res+=(sign*num);
+                sign=1;
+                num=0;
+                int prev_sign=st.top();
+                st.pop();
+                int prev_res=st.top();
+                st.pop();
+                res*=prev_sign;
+                res+=prev_res;
+            }
+            
         }
-        result += (sign*number);
-        return result;
+        res+=(num*sign);
+        return res;
     }
 };
